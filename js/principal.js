2bbcb582220753351204f6a0dca85b170aab4f32
maitte <maitena_escalada@hotmail.com>
2022-04-28 16:44:57 -0300

    add validaciones

--- js/principal.js
+++ js/principal.js
@@ -9,6 +9,25 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
+var tdIMC = paciente.querySelector(".info-imc");
+
+var esPesoValido = true;
+var esAlturaValida = true;
+
+if (peso <= 0 || peso > 700){
+    esPesoValido = false;
+    tdIMC.textContent = "Peso incorrecto";
+} 
+
+if (altura <= 0 || altura > 3.0){
+    esAlturaValida = false;
+    tdIMC.textContent = "Altura incorrecta";
+}
+
+if (esPesoValido && esAlturaValida){
+    tdIMC.textContent = calcularIMC(peso, altura);
+}
+
 function calcularIMC(peso, altura){
     return peso/(altura * altura);
 }
