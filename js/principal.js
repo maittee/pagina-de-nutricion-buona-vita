c725e1eeb5dbf8abecd03275bcc8b3343a0691cb
maitte <maitena_escalada@hotmail.com>
2022-04-28 16:18:36 -0300

    add funcion calcularIMC

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,14 @@
 var titulo = document.querySelector(".titulo-principal");
 titulo.textContent = "Buena Vida Nutrición";
+
+var paciente = document.querySelector("#primer-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+function calcularIMC(peso, altura){
+    return peso/(altura * altura);
+}
