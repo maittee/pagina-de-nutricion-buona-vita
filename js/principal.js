a25ae0673933e1e3d0f6f35d9ac3e4b4a8523bc6
maitte <maitena_escalada@hotmail.com>
2022-04-28 17:02:38 -0300

    add esPesoValido y esAlturaValida

--- js/principal.js
+++ js/principal.js
@@ -1,33 +1,42 @@
 var titulo = document.querySelector(".titulo-principal");
 titulo.textContent = "Buena Vida Nutrición";
 
-var paciente = document.querySelector("#primer-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdIMC = paciente.querySelector(".info-imc");
-
-var esPesoValido = true;
-var esAlturaValida = true;
-
-if (peso <= 0 || peso > 700){
-    esPesoValido = false;
-    tdIMC.textContent = "Peso incorrecto";
-} 
-
-if (altura <= 0 || altura > 3.0){
-    esAlturaValida = false;
-    tdIMC.textContent = "Altura incorrecta";
-}
-
-if (esPesoValido && esAlturaValida){
-    tdIMC.textContent = calcularIMC(peso, altura);
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdIMC = paciente.querySelector(".info-imc"); 
+    
+    if (esPesoValido(peso) && esAlturaValida(altura)){
+        tdIMC.textContent = calcularIMC(peso, altura);
+    }
 }
 
 function calcularIMC(peso, altura){
-    return peso/(altura * altura);
+    return (peso/(altura * altura)).toFixed(2);
+}
+
+function esPesoValido(peso){
+    var esPesoValido = true;
+    if (peso <= 0 || peso > 700){
+        esPesoValido = false;
+        tdIMC.textContent = "Peso incorrecto";
+    }
+    return esPesoValido;
+}
+
+function esAlturaValida(altura){
+    var esAlturaValida = true;
+    if (altura <= 0 || altura > 3.0){
+        esAlturaValida = false;
+        tdIMC.textContent = "Altura incorrecta";
+    }
+    return esAlturaValida;
 }
