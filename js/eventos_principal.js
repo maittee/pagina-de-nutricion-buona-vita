4718395ebb7858ec592653309a665dae5a5826f4
maitte <maitena_escalada@hotmail.com>
2022-04-28 19:24:42 -0300

    adicionando pacientes en la tabla

--- js/eventos_principal.js
+++ js/eventos_principal.js
@@ -2,4 +2,33 @@ var btnAdicionar = document.querySelector("#adicionar-paciente");
 
 btnAdicionar.addEventListener("click", function(event){
     event.preventDefault();
+    
+    var form = document.querySelector("#form-adicionar");
+    
+    var nombre = form.nombre.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+    var tabla = documente.querySelector("#tabla-pacientes");
+    
+    var pacienteTr = document.createElement("tr");
+    var nombreTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+    
+    nombreTd.textContent = nombre;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    
+    tabla.appendChild(pacienteTr);
+    
 });
