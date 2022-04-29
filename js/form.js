956d6576e0753a6fafba909a79585808d1a91c96
maitte <maitena_escalada@hotmail.com>
2022-04-28 21:49:53 -0300

    add contruirTr

--- js/form.js
+++ js/form.js
@@ -5,30 +5,8 @@ btnAdicionar.addEventListener("click", function(event){
     
     var form = document.querySelector("#form-adicionar");
     var paciente = capturarDatosDePaciente(form);
+    var pacienteTr = construirTr(paciente);
     var tabla = document.querySelector("#tabla-pacientes");
-    
-    // creacion de tds y un tr
-    var pacienteTr = document.createElement("tr");
-    var nombreTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-    
-    // asignar los valores a la propiedad textContent
-    nombreTd.textContent = paciente.nombre;
-    pesoTd.textContent = paciente.peso;
-    alturaTd.textContent = paciente.altura;
-    gorduraTd.textContent = paciente.gordura;
-    imcTd.textContent = calcularIMC(peso, altura);
-    
-    // asignacion al tr de los td, y a tabla el tr
-    pacienteTr.appendChild(nombreTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-    
     tabla.appendChild(pacienteTr);
 });
 
@@ -39,6 +17,44 @@ function capturarDatosDePaciente(form){
         peso: form.peso.value,
         altura: form.altura.value,
         gordura: form.gordura.value,
+        imc: calcularIMC(form.peso.value, form.altura.value)
     }
     return paciente;
+}
+
+function construirTr(paciente){
+    // creacion de tds y un tr
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+    
+    var nombreTd = document.createElement("td");
+    nombreTd.classList.add("info-nombre");
+    
+    var pesoTd = document.createElement("td");
+    pesoTd.classList.add("info-peso");
+    
+    var alturaTd = document.createElement("td");
+    alturaTd.classList.add("info-altura");
+    
+    var gorduraTd = document.createElement("td");
+    gorduraTd.classList.add("info-gordura");
+    
+    var imcTd = document.createElement("td");
+    imcTd.classList.add("info-imc");
+    
+    // asignar los valores a la propiedad textContent
+    nombreTd.textContent = paciente.nombre;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
+    
+    // asignacion al tr de los td, y a tabla el tr
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+    
+    return pacienteTr;
 }
