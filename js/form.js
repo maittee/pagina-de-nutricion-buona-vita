b59226a3d5b6891d5d51bcc64c5e57dee1d620f5
maitte <maitena_escalada@hotmail.com>
2022-04-28 21:33:31 -0300

    add objeto paciente y capturarDatosDePaciente

--- js/form.js
+++ js/form.js
@@ -4,14 +4,10 @@ btnAdicionar.addEventListener("click", function(event){
     event.preventDefault();
     
     var form = document.querySelector("#form-adicionar");
+    var paciente = capturarDatosDePaciente(form);
+    var tabla = document.querySelector("#tabla-pacientes");
     
-    var nombre = form.nombre.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-    
-    var tabla = documente.querySelector("#tabla-pacientes");
-    
+    // creacion de tds y un tr
     var pacienteTr = document.createElement("tr");
     var nombreTd = document.createElement("td");
     var pesoTd = document.createElement("td");
@@ -19,16 +15,30 @@ btnAdicionar.addEventListener("click", function(event){
     var gorduraTd = document.createElement("td");
     var imcTd = document.createElement("td");
     
-    nombreTd.textContent = nombre;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
+    // asignar los valores a la propiedad textContent
+    nombreTd.textContent = paciente.nombre;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = calcularIMC(peso, altura);
     
+    // asignacion al tr de los td, y a tabla el tr
     pacienteTr.appendChild(nombreTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
     
     tabla.appendChild(pacienteTr);
-    
 });
+
+function capturarDatosDePaciente(form){
+    // captura de los datos del formulario
+    var paciente = {
+        nombre: form.nombre.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+    }
+    return paciente;
+}
